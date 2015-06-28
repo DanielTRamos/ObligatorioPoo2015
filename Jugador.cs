760f379024518fc1f29a54ca9629c1e827fde606
luistito <emilio_tito@hotmail.com>
2015-06-28 10:22:21 -0300

    001

--- Jugador.cs
+++ Jugador.cs
@@ -5,7 +5,103 @@ using System.Text;
 
 namespace ObligatorioPoo2015
 {
-    class Jugador
+    class Jugador: IJugador
     {
+        private string posicion;
+        private string nombre;
+        private string apellido;
+        private int edad;
+        private string Posición;
+        private int Patear;
+        private int Cabecear;
+        private int Atajar;
+        private int Regatear;
+        private int TirarTiroLibre;
+        private int GolesHechos;
+
+        public string Posición
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Patear
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Cabecear
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Atajar
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int Regatear
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int TirarTiroLibre
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public int GolesHechos
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Nombre
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public string Apellido
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
+
+        public int Edad
+        {
+            get
+            {
+                throw new NotImplementedException();
+            }
+            set
+            {
+                throw new NotImplementedException();
+            }
+        }
     }
 }
